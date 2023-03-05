712876811bd35eadc07c196c97aa26c97c526f2e
Abdelrahman Ghazaly <ghazaly.abdelrahman@gmail.com>
2023-03-05 19:04:39 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zigy_assignment/features/home/bottom_nav_bar.dart';
+import 'package:zigy_assignment/features/load_users/load_users_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNavBar(),
+      home: LoadUsersScreen(),
     );
   }
 }
